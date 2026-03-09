e7876ee33c02f251fe54391f0d696135583ecb0f
TangerineBoi <45380138+TangerineBoi@users.noreply.github.com>
2026-03-08 20:07:36 -0700

    Enhance I2C setup and data reading in turret sketch
    Added I2C communication setup and data handling for the turret.

--- WIPTurretSketch.ino
+++ WIPTurretSketch.ino
@@ -6,18 +6,35 @@ void setup() {
   // put your setup code here, to run once:
   Wire.begin(0x68);
   baseServo.attach(2);
+  //baseServo.write(90);
   Serial.begin(9600);
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B);
+  Wire.write(0);  //turn on
+  Wire.write(0x1B);
+  Wire.write(0x06);
+  Wire.endTransmission(true);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("from register: ");
   Wire.beginTransmission(0x68);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Serial.println(Wire.endTransmission(false));
+  Wire.write(0x3B); // X val
+  Wire.requestFrom(0x3B, 14);
+  Wire.endTransmission(true);
+  
+  /////
 
-  Wire.requestFrom(0x68,1);
-  Wire.onReceive(1);
+  Serial.println("request: ");
+  Wire.beginTransmission(0x68);
+  Wire.read();
+
+  Wire.endTransmission(true);
   Serial.println(Wire.read());
-  delay(500);
+  
+  if(Wire.read() <= 180){
+  baseServo.write(Wire.read());
+  }
+  delay(100);
 }

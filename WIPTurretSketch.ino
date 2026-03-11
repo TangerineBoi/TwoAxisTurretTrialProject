d324c2e88c8da12a25e8ddec40d2a025d1f5d8a2
TangerineBoi <45380138+TangerineBoi@users.noreply.github.com>
2026-03-11 01:32:56 -0700

    Improve I2C read and servo write functionality, servo moving but strangely
    Refactor I2C communication and servo control logic.

--- WIPTurretSketch.ino
+++ WIPTurretSketch.ino
@@ -1,7 +1,8 @@
 #include <Servo.h>
 Servo baseServo;
 #include <Wire.h>
-
+int wireReadPrevious = Wire.read();
+int counter = 0;
 void setup() {
   // put your setup code here, to run once:
   Wire.begin(0x68);
@@ -9,32 +10,34 @@ void setup() {
   //baseServo.write(90);
   Serial.begin(9600);
   Wire.beginTransmission(0x68);
-  Wire.write(0x6B);
+  Wire.write(0x6B); //turn on
   Wire.write(0);  //turn on
-  Wire.write(0x1B);
-  Wire.write(0x06);
+  //Wire.write(0x1B);
+  //Wire.write(0x06);
   Wire.endTransmission(true);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("from register: ");
+
   Wire.beginTransmission(0x68);
-  Wire.write(0x3B); // X val
-  Wire.requestFrom(0x3B, 14);
-  Wire.endTransmission(true);
-  
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
   /////
+  Wire.requestFrom(0x68, 4);
+  Serial.print("read: ");
 
-  Serial.println("request: ");
-  Wire.beginTransmission(0x68);
-  Wire.read();
+  counter++;
 
-  Wire.endTransmission(true);
-  Serial.println(Wire.read());
-  
-  if(Wire.read() <= 180){
-  baseServo.write(Wire.read());
+  if (Wire.available() && counter == 2){
+    Serial.println(Wire.read());
+    wireReadPrevious = Wire.read();
+    baseServo.write(Wire.read());
+    counter = 0;
+    delay(10);
   }
-  delay(100);
+  
+  
+  delay(200);
 }
